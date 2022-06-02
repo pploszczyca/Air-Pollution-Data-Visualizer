10134edf6831cd01401b9401125117ebd9bcc7de
kkoz34 <jkozlak@student.agh.edu.pl>
2022-06-02 07:19:32 +0200

    [APDV-47] extracting methods

--- frontend/lib/App.dart
+++ frontend/lib/App.dart
@@ -28,64 +28,73 @@ class _AppState extends State<App> {
   Widget build(BuildContext context) {
     MediaQueryData queryData;
     queryData = MediaQuery.of(context);
-    Widget navigationBar;
-
+    
     if (queryData.size.width > 560) {
-      navigationBar = Expanded(
-        flex: 1,
-        child: NavigationRail(
-          selectedIndex: _selectedIndex,
-          onDestinationSelected: (int index) {
-            setState(() {
-              _selectedIndex = index;
-            });
-          },
-          labelType: NavigationRailLabelType.selected,
-          destinations: [
-            _buildRailNavigationItem("Endpoint List"),
-            _buildRailNavigationItem("Compare Endpoints"),
-            _buildRailNavigationItem("Profile"),
-          ],
-        ),
-      );
-      return Scaffold(
-          body: Row(
-        children: [
-          Container(
-            width: 100,
-              child: navigationBar,),
-          const VerticalDivider(thickness: 1, width: 1),
-          // line splitting navbar and main content
-          Expanded(
-            child: IndexedStack(
-              index: _selectedIndex,
-              children: _navigationOptions,
-            ),
-          ),
-        ],
-      ));
-    } else {
-      navigationBar = BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: <BottomNavigationBarItem>[
-          _buildNavigationItem("Endpoint List"),
-          _buildNavigationItem("Compare Endpoints"),
-          _buildNavigationItem("Profile"),
-        ],
-        onTap: (index) => setState(() {
-          _selectedIndex = index;
-        }),
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.pink,
-      );
-      return Scaffold(
-        body: IndexedStack(
-          index: _selectedIndex,
-          children: _navigationOptions,
-        ),
-        bottomNavigationBar: navigationBar,
-      );
+      return _buildRailNavigationScaffold();
     }
+    else {
+      return _buildBottomNavigationScaffold();
+    }
+  }
+  
+  Scaffold _buildRailNavigationScaffold() {
+    Widget _navi = Expanded(
+      flex: 1,
+      child: NavigationRail(
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: (int index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        labelType: NavigationRailLabelType.selected,
+        destinations: [
+          _buildRailNavigationItem("Endpoint List"),
+          _buildRailNavigationItem("Compare Endpoints"),
+          _buildRailNavigationItem("Profile"),
+        ],
+      ),
+    );
+    return Scaffold(
+        body: Row(
+          children: [
+            SizedBox(
+              width: 100,
+              child: _navi,),
+            const VerticalDivider(thickness: 1, width: 1),
+            // line splitting navbar and main content
+            Expanded(
+              child: IndexedStack(
+                index: _selectedIndex,
+                children: _navigationOptions,
+              ),
+            ),
+          ],
+        ));
+  }
+
+  Scaffold _buildBottomNavigationScaffold() {
+    Widget _navBar = BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      items: <BottomNavigationBarItem>[
+        _buildNavigationItem("Endpoint List"),
+        _buildNavigationItem("Compare Endpoints"),
+        _buildNavigationItem("Profile"),
+      ],
+      onTap: (index) =>
+          setState(() {
+            _selectedIndex = index;
+          }),
+      currentIndex: _selectedIndex,
+      selectedItemColor: Colors.pink,
+    );
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _navigationOptions,
+      ),
+      bottomNavigationBar: _navBar,
+    );
   }
 
   NavigationRailDestination _buildRailNavigationItem(String stringLabel) {

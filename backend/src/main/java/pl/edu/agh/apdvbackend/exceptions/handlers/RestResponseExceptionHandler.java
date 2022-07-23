8a17b352dfe04b7f604aa63ce422543b7cabb0d2
pploszczyca <piotrus.plo@gmail.com>
2022-07-23 12:10:53 +0200

    [APDV-94] Unused import removed

--- backend/src/main/java/pl/edu/agh/apdvbackend/exceptions/handlers/RestResponseExceptionHandler.java
+++ backend/src/main/java/pl/edu/agh/apdvbackend/exceptions/handlers/RestResponseExceptionHandler.java
@@ -1,6 +1,5 @@
 package pl.edu.agh.apdvbackend.exceptions.handlers;
 
-import com.auth0.jwt.exceptions.TokenExpiredException;
 import java.util.NoSuchElementException;
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;

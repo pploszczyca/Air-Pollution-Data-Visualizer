d1171d2f7096c12447f429dd0eb441dcaf3cd3b0
zbrze <westpulveris@gmail.com>
2022-07-31 21:58:43 +0200

    [APDV-83] regex changes

--- frontend/lib/Logging/utils.dart
+++ frontend/lib/Logging/utils.dart
@@ -1,4 +1,3 @@
-
 enum ButtonState { enabled, disabled }
 
 enum FormType { signin, signup }
@@ -6,26 +5,20 @@ enum FormType { signin, signup }
 bool checkPasswordRegex(String text) {
   const String pattern = r'^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{1,}$';
   final RegExp regex = RegExp(pattern);
-  return executeRegex(regex, text);
+  return regex.hasMatch(text);
 }
 
 bool checkEmailRegex(String text) {
   const String pattern =
       r'^(?=.{1,64}@)[A-Za-z0-9_-]+(.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(.[A-Za-z0-9-]+)*(.[A-Za-z]{2,})$';
   final RegExp regex = RegExp(pattern);
-  return executeRegex(regex, text);
+  return regex.hasMatch(text);
 }
 
-bool executeRegex(RegExp regex, String text) => regex.hasMatch(text);
-
-class AuthenticateForm{
+class AuthenticateForm {
   String email;
   String password;
   FormType formType;
 
-  AuthenticateForm(
-      this.email,
-      this.password,
-      this.formType
-      );
+  AuthenticateForm(this.email, this.password, this.formType);
 }

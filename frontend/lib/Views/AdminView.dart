45798e83b483b13c69861ce8cf2478a254bcdd85
zbrze <westpulveris@gmail.com>
2022-08-08 22:10:37 +0200

    [APDV-99]

--- frontend/lib/Views/AdminView.dart
+++ frontend/lib/Views/AdminView.dart
@@ -9,5 +9,42 @@ class AdminView extends StatefulWidget {
 
 class _AdminViewState extends State<AdminView> {
   @override
-  Widget build(BuildContext context) => const Text("ADMIN PANEL");
+  Widget build(BuildContext context) =>
+      Scaffold(appBar: _buildAppBar(), body: const SingleChildScrollView(
+        child: _buildGroupList(),
+      ));
+
+  PreferredSize _buildAppBar() => PreferredSize(
+      preferredSize: const Size.fromHeight(120),
+      child: AppBar(
+          centerTitle: true,
+          toolbarHeight: 120,
+          title: Container(
+              padding: const EdgeInsets.only(top: 20, bottom: 10),
+              child: const Text("Administrator panel")),
+          backgroundColor: Colors.white,
+          titleTextStyle: const TextStyle(
+              color: Colors.black,
+              fontFamily: 'Ubuntu Condensed',
+              fontSize: 40,
+              fontWeight: FontWeight.w500),
+          titleSpacing: 20,
+          bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(20),
+              child: Container(
+                  padding: const EdgeInsets.only(bottom: 10, top: 5),
+                  child: const Text(
+                    "User groups",
+                    style: TextStyle(fontSize: 25),
+                  )))));
+
+  ListView _buildGroupList() => ListView.builder(
+      itemCount: 5,
+      itemBuilder: (BuildContext context, int index) => ListTile(
+          leading: const Icon(Icons.list),
+          trailing: const Text(
+            "GFG",
+            style: TextStyle(color: Colors.green, fontSize: 15),
+          ),
+          title: Text("Group number $index")));
 }

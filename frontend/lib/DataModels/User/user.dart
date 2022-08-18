10c88450c16fae32005f927d68d47e65843e3eb6
kkoz34 <jkozlak@student.agh.edu.pl>
2022-08-18 22:33:59 +0200

    [APDV-108] adding secure storage

--- frontend/lib/DataModels/User/user.dart
+++ frontend/lib/DataModels/User/user.dart
@@ -1,18 +1,33 @@
 import 'package:adpv_frontend/DataModels/User/auth_token_response.dart';
-import 'package:adpv_frontend/DataModels/User/user_role.dart';
 import 'package:jwt_decode/jwt_decode.dart';
+
+enum UserRole {
+  user,
+  admin,
+  empty,
+}
 
 class User {
   late AuthTokenResponse tokenResponse;
   late String email;
-  List<UserRole> userRole = [];
+  List<UserRole> userRoles = [];
 
   User(this.tokenResponse) {
     final Map<String, dynamic> parsed = Jwt.parseJwt(tokenResponse.accessToken);
     email = parsed['sub'];
     for (var element in (parsed['roles'] as List)) {
-      userRole.add(UserRoleExtension.userRole(element));
+      userRoles.add(fromString(element));
     }
+  }
+
+  UserRole fromString(String roleString) {
+    switch (roleString) {
+      case "USER":
+        return UserRole.user;
+      case "ADMIN":
+        return UserRole.admin;
+    }
+    return UserRole.empty;
   }
 
   User.empty()
@@ -20,5 +35,5 @@ class User {
         email = '';
 
   @override
-  String toString() => 'User{email: $email, userRole: ${userRole.toString()}';
+  String toString() => 'User{email: $email, userRole: ${userRoles.toString()}';
 }

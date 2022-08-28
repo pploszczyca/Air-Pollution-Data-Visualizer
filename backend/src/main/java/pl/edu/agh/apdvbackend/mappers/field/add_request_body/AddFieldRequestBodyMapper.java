9e20ef5e3ec45d911e83ee3ae9712e2229d67777
pploszczyca <piotrus.plo@gmail.com>
2022-08-28 14:29:56 +0200

    [APDV-111] Added UnitNameMapperTest

--- backend/src/main/java/pl/edu/agh/apdvbackend/mappers/field/add_request_body/AddFieldRequestBodyMapper.java
+++ backend/src/main/java/pl/edu/agh/apdvbackend/mappers/field/add_request_body/AddFieldRequestBodyMapper.java
@@ -1,6 +1,5 @@
 package pl.edu.agh.apdvbackend.mappers.field.add_request_body;
 
-import java.util.Optional;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
@@ -8,23 +7,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import pl.edu.agh.apdvbackend.mappers.unit.UnitNameMapper;
 import pl.edu.agh.apdvbackend.models.body_models.field.AddFieldRequestBody;
 import pl.edu.agh.apdvbackend.models.database.Field;
-import pl.edu.agh.apdvbackend.models.database.Unit;
 
 @Mapper(componentModel = "spring")
 public abstract class AddFieldRequestBodyMapper {
 
     @Autowired
-    private UnitNameMapper unitNameMapper;
+    protected UnitNameMapper unitNameMapper;
 
-    @Mapping(target = "unit", expression = "java(toUnit(requestBody.unitName()))")
+    @FieldMapping
     public abstract Field toField(AddFieldRequestBody requestBody);
 
-    @Mapping(target = "unit", expression = "java(toUnit(requestBody.unitName()))")
+    @FieldMapping
     public abstract void updateFieldBy(AddFieldRequestBody requestBody, @MappingTarget Field field);
 
-    protected Unit toUnit(Optional<String> unitName) {
-        return unitName
-                .map(s -> unitNameMapper.toUnit(s))
-                .orElse(null);
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "groupEndpoint", ignore = true)
+    @Mapping(target = "unit", expression = "java(unitNameMapper.toUnit(requestBody.unitName()))")
+    private @interface FieldMapping{
     }
 }
---
+++ backend/src/test/java/pl/edu/agh/apdvbackend/mappers/unit/UnitNameMapperTest.java
@@ -0,0 +1,55 @@
+package pl.edu.agh.apdvbackend.mappers.unit;
+
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import pl.edu.agh.apdvbackend.fakes.UnitFakes;
+import pl.edu.agh.apdvbackend.repositories.UnitRepository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.doReturn;
+
+@SpringBootTest
+class UnitNameMapperTest {
+
+    @SpyBean
+    private UnitRepository unitRepository;
+
+    @Autowired
+    private UnitNameMapper mapper;
+
+    @Test
+    public void ShouldReturnUnit_WhenOptionalIsNotEmpty() {
+        // Given
+        final String unitName = "unitName";
+        final var unitNameOptional = Optional.of(unitName);
+        final var unit = UnitFakes
+                .builder()
+                .name(unitName)
+                .build();
+
+        doReturn(Optional.of(unit))
+                .when(unitRepository).findByName(unitName);
+
+        // When
+        final var result = mapper.toUnit(unitNameOptional);
+
+        // Then
+        assertEquals(unit, result);
+    }
+
+    @Test
+    public void ShouldReturnNull_WhenOptionalIsEmpty() {
+        // Given
+        final Optional<String> unitNameOptional = Optional.empty();
+
+        // When
+        final var result = mapper.toUnit(unitNameOptional);
+
+        // Then
+        assertNull(result);
+    }
+}

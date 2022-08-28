4b16a76d42a92cc23ab3a813cfabb41766400549
pploszczyca <piotrus.plo@gmail.com>
2022-08-28 18:16:19 +0200

    [APDV-111] Added FieldParserMapMapper

--- backend/src/main/java/pl/edu/agh/apdvbackend/mappers/EndpointMapper.java
+++ backend/src/main/java/pl/edu/agh/apdvbackend/mappers/EndpointMapper.java
@@ -1,20 +1,16 @@
 package pl.edu.agh.apdvbackend.mappers;
 
 import java.util.List;
-import java.util.Map;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.springframework.beans.factory.annotation.Autowired;
 import pl.edu.agh.apdvbackend.mappers.field_parser.FieldAndParserKeyMapper;
-import pl.edu.agh.apdvbackend.mappers.field_parser.FieldAndParserMapper;
+import pl.edu.agh.apdvbackend.mappers.field_parser.FieldParserMapMapper;
 import pl.edu.agh.apdvbackend.models.body_models.endpoint.AddEndpointRequestBody;
 import pl.edu.agh.apdvbackend.models.body_models.endpoint.EndpointSummaryResponseBody;
 import pl.edu.agh.apdvbackend.models.body_models.endpoint.UserEndpointResponseBody;
-import pl.edu.agh.apdvbackend.models.body_models.field_and_parser.FieldAndParser;
 import pl.edu.agh.apdvbackend.models.database.Endpoint;
-import pl.edu.agh.apdvbackend.models.database.Field;
-import pl.edu.agh.apdvbackend.models.database.FieldParser;
 
 @Mapper(componentModel = "spring")
 public abstract class EndpointMapper {
@@ -23,7 +19,7 @@ public abstract class EndpointMapper {
     protected FieldAndParserKeyMapper fieldAndParserKeyMapper;
 
     @Autowired
-    private FieldAndParserMapper fieldAndParserMapper;
+    protected FieldParserMapMapper fieldParserMapMapper;
 
     @Mapping(
             target = "fieldParserMap",
@@ -46,21 +42,13 @@ public abstract class EndpointMapper {
     public abstract List<UserEndpointResponseBody> endpointListToUserEndpointResponseBodyList(
             List<Endpoint> endpoints);
 
-    @Mapping(target = "fieldAndParserList", expression = "java(fieldAndParserMapToList(endpoint.getFieldParserMap()))")
+    @Mapping(
+            target = "fieldAndParserList",
+            expression = "java(fieldParserMapMapper.toFieldAndParserList(endpoint.getFieldParserMap()))"
+    )
     public abstract EndpointSummaryResponseBody endpointToSummaryResponseBody(
             Endpoint endpoint);
 
     public abstract List<EndpointSummaryResponseBody> endpointListToSummaryResponseBodyList(
             List<Endpoint> endpoints);
-
-    protected List<FieldAndParser> fieldAndParserMapToList(
-            Map<Field, FieldParser> fieldParserMap) {
-        return fieldParserMap
-                .entrySet()
-                .stream()
-                .map(fieldFieldParserEntry -> fieldAndParserMapper.combineFieldAndParser(
-                        fieldFieldParserEntry.getKey(),
-                        fieldFieldParserEntry.getValue()))
-                .toList();
-    }
 }

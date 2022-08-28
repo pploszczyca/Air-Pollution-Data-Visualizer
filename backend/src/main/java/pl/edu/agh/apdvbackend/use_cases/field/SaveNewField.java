05483999c660713fe1346fb3a3eb6756c6bc3ae8
pploszczyca <piotrus.plo@gmail.com>
2022-08-28 16:56:06 +0200

    [APDV-111] Removed unused imports

--- backend/src/main/java/pl/edu/agh/apdvbackend/use_cases/field/SaveNewField.java
+++ backend/src/main/java/pl/edu/agh/apdvbackend/use_cases/field/SaveNewField.java
@@ -2,8 +2,6 @@ package pl.edu.agh.apdvbackend.use_cases.field;
 
 import pl.edu.agh.apdvbackend.models.body_models.field.AddFieldRequestBody;
 import pl.edu.agh.apdvbackend.models.database.Field;
-import pl.edu.agh.apdvbackend.models.database.FieldType;
-import pl.edu.agh.apdvbackend.models.database.Unit;
 
 public interface SaveNewField {
     Field execute(AddFieldRequestBody addFieldRequestBody);

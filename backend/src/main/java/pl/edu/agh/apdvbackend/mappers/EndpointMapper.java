e5bf0443931eac3e80da2de734835c6ee12a5f05
pploszczyca <piotrus.plo@gmail.com>
2022-08-31 20:12:58 +0200

    [APDV-111] Removed unused methods in EndpointMapper

--- backend/src/main/java/pl/edu/agh/apdvbackend/mappers/EndpointMapper.java
+++ backend/src/main/java/pl/edu/agh/apdvbackend/mappers/EndpointMapper.java
@@ -1,13 +1,11 @@
 package pl.edu.agh.apdvbackend.mappers;
 
-import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.springframework.beans.factory.annotation.Autowired;
 import pl.edu.agh.apdvbackend.mappers.field_parser.FieldAndParserKeyMapper;
 import pl.edu.agh.apdvbackend.models.body_models.endpoint.AddEndpointRequestBody;
-import pl.edu.agh.apdvbackend.models.body_models.endpoint.UserEndpointResponseBody;
 import pl.edu.agh.apdvbackend.models.database.Endpoint;
 
 @Mapper(componentModel = "spring")
@@ -30,10 +28,4 @@ public abstract class EndpointMapper {
             AddEndpointRequestBody requestBody,
             @MappingTarget Endpoint endpoint
     );
-
-    public abstract UserEndpointResponseBody endpointToUserEndpointResponseBody(
-            Endpoint endpoint);
-
-    public abstract List<UserEndpointResponseBody> endpointListToUserEndpointResponseBodyList(
-            List<Endpoint> endpoints);
 }

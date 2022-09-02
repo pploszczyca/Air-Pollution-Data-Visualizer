2a507ef8087f49d048ba48c28964d51586a4d0d7
pploszczyca <piotrus.plo@gmail.com>
2022-09-02 19:46:02 +0200

    [APDV-111] Moved EndpointMapper

--- backend/src/main/java/pl/edu/agh/apdvbackend/mappers/endpoint/EndpointMapper.java
+++ backend/src/main/java/pl/edu/agh/apdvbackend/mappers/endpoint/EndpointMapper.java
@@ -1,4 +1,4 @@
-package pl.edu.agh.apdvbackend.mappers;
+package pl.edu.agh.apdvbackend.mappers.endpoint;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
--- backend/src/main/java/pl/edu/agh/apdvbackend/use_cases/endpoint/SaveNewEndpointImpl.java
+++ backend/src/main/java/pl/edu/agh/apdvbackend/use_cases/endpoint/SaveNewEndpointImpl.java
@@ -2,7 +2,7 @@ package pl.edu.agh.apdvbackend.use_cases.endpoint;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import pl.edu.agh.apdvbackend.mappers.EndpointMapper;
+import pl.edu.agh.apdvbackend.mappers.endpoint.EndpointMapper;
 import pl.edu.agh.apdvbackend.models.body_models.endpoint.AddEndpointRequestBody;
 import pl.edu.agh.apdvbackend.models.database.Endpoint;
 import pl.edu.agh.apdvbackend.repositories.EndpointRepository;

b40f6844bc02935df76cc05f1cccc0d0a662cc3b
pploszczyca <piotrus.plo@gmail.com>
2022-09-06 21:02:34 +0200

    [APDV-111] Changed name from AddEndpointRequestBody to EndpointRequestBody

--- backend/src/main/java/pl/edu/agh/apdvbackend/controllers/EndpointController.java
+++ backend/src/main/java/pl/edu/agh/apdvbackend/controllers/EndpointController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pl.edu.agh.apdvbackend.models.body_models.Response;
-import pl.edu.agh.apdvbackend.models.body_models.endpoint.AddEndpointRequestBody;
+import pl.edu.agh.apdvbackend.models.body_models.endpoint.EndpointRequestBody;
 import pl.edu.agh.apdvbackend.models.body_models.endpoint.EndpointResponseBody;
 import pl.edu.agh.apdvbackend.models.body_models.endpoint.EndpointWithField;
 import pl.edu.agh.apdvbackend.models.body_models.endpoint.UserEndpointResponseBody;
@@ -80,8 +80,8 @@ public class EndpointController {
             security = @SecurityRequirement(name = JWT_AUTH)
     )
     @PostMapping
-    public Response<Endpoint> addEndpoint(@RequestBody AddEndpointRequestBody addEndpointRequestBody) {
-        return endpointService.addEndpoint(addEndpointRequestBody);
+    public Response<Endpoint> addEndpoint(@RequestBody EndpointRequestBody endpointRequestBody) {
+        return endpointService.addEndpoint(endpointRequestBody);
     }
 
     @Operation(
@@ -99,10 +99,10 @@ public class EndpointController {
     )
     @PutMapping
     public Response<Endpoint> updateEndpoint(
-            @RequestBody AddEndpointRequestBody addEndpointRequestBody,
+            @RequestBody EndpointRequestBody endpointRequestBody,
             @RequestParam Long endpointId
     ) {
-        return endpointService.updateEndpoint(addEndpointRequestBody,
+        return endpointService.updateEndpoint(endpointRequestBody,
                 endpointId);
     }
 }

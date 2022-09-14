d4eb2c29dde8352ca531f1ee3cbf27c2f188e33c
pploszczyca <piotrus.plo@gmail.com>
2022-09-14 20:12:31 +0200

    [APDV-111] Added UserResponseBodyMapper

--- backend/src/main/java/pl/edu/agh/apdvbackend/use_cases/user/GetAboutUserImpl.java
+++ backend/src/main/java/pl/edu/agh/apdvbackend/use_cases/user/GetAboutUserImpl.java
@@ -2,18 +2,18 @@ package pl.edu.agh.apdvbackend.use_cases.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import pl.edu.agh.apdvbackend.mappers.UserMapper;
+import pl.edu.agh.apdvbackend.mappers.user.UserResponseBodyMapper;
 import pl.edu.agh.apdvbackend.models.body_models.user.UserResponseBody;
 
 @Component
 @RequiredArgsConstructor
 public class GetAboutUserImpl implements GetAboutUser {
 
-    private final UserMapper userMapper;
+    private final UserResponseBodyMapper userMapper;
     private final GetUser getUser;
 
     @Override
     public UserResponseBody execute(Long userId) {
-        return userMapper.userToAboutResponseBody(getUser.execute(userId));
+        return userMapper.map(getUser.execute(userId));
     }
 }

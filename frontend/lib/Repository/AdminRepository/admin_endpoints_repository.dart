31374ad0809c23f7a544397b2a331457bd4135b8
kkoz34 <jkozlak@student.agh.edu.pl>
2022-10-25 01:05:58 +0200

    [APDV-117] adding single endpoint admin view

--- frontend/lib/Repository/AdminRepository/admin_endpoints_repository.dart
+++ frontend/lib/Repository/AdminRepository/admin_endpoints_repository.dart
@@ -1,27 +1,71 @@
 import 'package:adpv_frontend/Common/urls.dart';
+import 'package:adpv_frontend/DataModels/enable_field.dart';
+import 'package:adpv_frontend/DataModels/field_parser.dart';
 import 'package:adpv_frontend/Repository/UserRepository/user_gateway.dart';
 import 'package:dio/dio.dart';
 
 import '../../DataModels/backend_response.dart';
 import '../UserRepository/auth_gateway.dart';
 
+class EndpointAdminField {
+  int fieldId;
+  int parserId;
+  String label;
+  String path;
+
+  EndpointAdminField(this.fieldId, this.parserId, this.label, this.path);
+
+  EndpointAdminField.fromJson(Map json)
+      : fieldId = json['fieldId'],
+        parserId = json['parserId'],
+        label = json['label'],
+        path = json['path'];
+}
+
 class EndpointAdminData {
   int id;
   int endpointNumber;
   String label;
+  String sensorUrl;
+  List<EndpointAdminField> fieldList;
 
-  EndpointAdminData(this.id, this.endpointNumber, this.label);
+  EndpointAdminData.fromJson(Map endpointDataJson)
+      : id = endpointDataJson['id'],
+        endpointNumber = endpointDataJson['endpointNumber'],
+        label = endpointDataJson['label'],
+        sensorUrl = endpointDataJson['sensorUrl'],
+        fieldList = endpointDataJson['fieldAndParserList']
+            // ignore: unnecessary_lambdas
+            .map<EndpointAdminField>((e) => EndpointAdminField.fromJson(e))
+            .toList();
 
-  EndpointAdminData.fromJson(Map json)
-      : id = json['id'],
-        endpointNumber = json['endpointNumber'],
-        label = json['label'];
+  Map<String, String> getEndpointBasicInfo() => {
+        "id": id.toString(),
+        "number": endpointNumber.toString(),
+        "label": label,
+        "sensorUrl": sensorUrl
+      };
+}
+
+class EndpointComplexData {
+  Map<int, FieldParser> fieldParsers;
+  Map<int, EnableField> enableFields;
+  List<EndpointAdminData> endpoints;
+
+  EndpointComplexData(this.fieldParsers, this.enableFields, this.endpoints);
 }
 
 class AdminEndpointsRepository {
   Dio _client = Dio();
 
   UserGateway userGateway = UserGateway();
+
+  Future<EndpointComplexData> getComplexData() async {
+    final List<EndpointAdminData> endpoints = await getAllEndpoints();
+    final Map<int, EnableField> fields = await getAllFields();
+    final Map<int, FieldParser> parsers = await getAllParsers();
+    return EndpointComplexData(parsers, fields, endpoints);
+  }
 
   Future<List<EndpointAdminData>> getAllEndpoints() async {
     _client = Dio();
@@ -39,10 +83,54 @@ class AdminEndpointsRepository {
             BackendResponse.fromJson(response.data);
         return backendResponse.data
             .map<EndpointAdminData>(
-          //ignore: unnecessary_lambdas
+              //ignore: unnecessary_lambdas
               (endpoint) => EndpointAdminData.fromJson(endpoint),
             )
             .toList();
+      }
+    }
+    return Future.error("Cannot get all endpoints list");
+  }
+
+  Future<Map<int, FieldParser>> getAllParsers() async {
+    _client = Dio();
+
+    final AuthResponse authResponse = await userGateway.getFromMemory();
+
+    if (authResponse.success) {
+      final String token = authResponse.tokens!.accessToken;
+      _client.options.headers["Authorization"] = "Bearer $token";
+
+      final Response response = await _client.get(backendURL + "field/parser");
+      if (response.statusCode == 200) {
+        final BackendResponse backendResponse =
+            BackendResponse.fromJson(response.data);
+
+        return {
+          for (var e in backendResponse.data) e['id']: FieldParser.fromJson(e)
+        };
+      }
+    }
+    return Future.error("Cannot get all endpoints list");
+  }
+
+  Future<Map<int, EnableField>> getAllFields() async {
+    _client = Dio();
+
+    final AuthResponse authResponse = await userGateway.getFromMemory();
+
+    if (authResponse.success) {
+      final String token = authResponse.tokens!.accessToken;
+      _client.options.headers["Authorization"] = "Bearer $token";
+
+      final Response response = await _client.get(backendURL + "field");
+      if (response.statusCode == 200) {
+        final BackendResponse backendResponse =
+            BackendResponse.fromJson(response.data);
+
+        return {
+          for (var e in backendResponse.data) e['id']: EnableField.fromJson(e)
+        };
       }
     }
     return Future.error("Cannot get all endpoints list");

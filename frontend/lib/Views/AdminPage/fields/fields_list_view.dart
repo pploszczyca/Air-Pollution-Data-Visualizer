36c238bb2803444e39ed36e11ccea2cacbe384e7
zbrze <58784685+zbrze@users.noreply.github.com>
2022-11-17 00:22:30 +0100

    Apdv 145 ekran dodawania fielda (#90)
    * [APDV-132] user list with edition
    
    * done
    
    * idk
    
    * first commit
    
    * commit bf swotch
    
    * done
    
    * adding + edit
    
    * edycja + dodawanie
    
    * lint
    
    * format
    
    * to jest mandela efekt, mówie wam
    
    * fixes
    
    Co-authored-by: kkoz34 <Bebok@123>
    Co-authored-by: zbrzezin <zbrzezin@akamai.com>

--- frontend/lib/Views/AdminPage/fields/fields_list_view.dart
+++ frontend/lib/Views/AdminPage/fields/fields_list_view.dart
@@ -1,3 +1,4 @@
+import 'package:adpv_frontend/Views/AdminPage/fields/show_add_field_modal.dart';
 import 'package:adpv_frontend/Widgets/common_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -7,6 +8,7 @@ import '../../../Providers/fields_list_provider.dart';
 import '../../../Repository/AdminRepository/fields_repository.dart';
 import '../../../Widgets/AdminWidgets/admin_app_bar.dart';
 import '../../../Widgets/AdminWidgets/admin_buttons.dart';
+import '../../../Widgets/AdminWidgets/admin_styles.dart';
 import '../../../Widgets/AdminWidgets/confirmation_dialog_modal.dart';
 import '../../../Widgets/AdminWidgets/group_card.dart';
 import '../../../Widgets/SortingWidgets/sort_bar.dart';
@@ -23,6 +25,7 @@ class FieldsListView extends StatefulWidget {
 
 class _FieldsListViewState extends State<FieldsListView> {
   late Future<List<FieldData>> fields;
+  late FieldsListProvider provider;
 
   @override
   void initState() {
@@ -32,128 +35,233 @@ class _FieldsListViewState extends State<FieldsListView> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: adminAppBar("Admin panel", "Users list"),
-        body: _buildBody(),
-      );
+    appBar: adminAppBar("Admin panel", "Users list"),
+    body: _buildBody(),
+    floatingActionButton: _buildAddButton(),
+  );
 
   Widget _buildBody() => FutureBuilder<List<FieldData>>(
-        future: fields,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState != ConnectionState.done) {
-            return loadingInCenter();
-          } else {
-            return ChangeNotifierProvider(
-              create: (context) => FieldsListProvider(fields),
-              child: Consumer<FieldsListProvider>(
-                builder: (context, provider, _) => RefreshIndicator(
-                  onRefresh: () => _onPullDownRefresh(provider),
-                  child: Column(
-                    children: [
-                      buildSortBar(
-                        provider.sortingModel,
+    future: fields,
+    builder: (context, snapshot) {
+      if (snapshot.connectionState != ConnectionState.done) {
+        return loadingInCenter();
+      } else {
+        return ChangeNotifierProvider(
+          create: (context) => provider = FieldsListProvider(fields),
+          child: Consumer<FieldsListProvider>(
+            builder: (context, provider, _) => RefreshIndicator(
+              onRefresh: () => _onPullDownRefresh(provider),
+              child: Column(
+                children: [
+                  buildSortBar(
+                    provider.sortingModel,
                         () => provider.notify(),
-                        provider.fieldsList,
-                        provider.getters,
-                      ),
-                      Expanded(
-                        child: _buildList(provider),
-                      ),
-                    ],
+                    provider.fieldsList,
+                    provider.getters,
                   ),
-                ),
+                  Expanded(
+                    child: _buildList(provider),
+                  ),
+                ],
               ),
-            );
-          }
-        },
-      );
+            ),
+          ),
+        );
+      }
+    },
+  );
 
   ListView _buildList(FieldsListProvider provider) => ListView(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20,
+    padding: const EdgeInsets.symmetric(
+      horizontal: 20,
+    ),
+    children: provider.fieldsList.map(_buildCard).toList(),
+  );
+
+  Card _buildCard(
+      FieldData fieldData,
+      ) =>
+      Card(
+        margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+        shadowColor: Colors.black,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
         ),
-        children: provider.fieldsList
-            .map(
-              (fieldData) => groupCard(
-                  Row(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.only(right: 10),
-                        child: Text(
-                          fieldData.id.toString(),
-                          style: const TextStyle(
-                            fontFamily: 'SofiaSans',
-                            fontSize: 25,
-                            color: Colors.black,
-                            fontWeight: FontWeight.normal,
-                          ),
-                        ),
-                      ),
-                      Flexible(
-                        child: Container(
-                          padding: const EdgeInsets.only(right: 10),
-                          child: Text(
-                            fieldData.name,
-                            overflow: TextOverflow.fade,
-                            softWrap: false,
-                            style: const TextStyle(
-                              fontFamily: 'SofiaSans',
-                              fontSize: 25,
-                              color: Colors.black,
-                              fontWeight: FontWeight.normal,
-                            ),
-                          ),
-                        ),
-                      )
-                    ],
+        child: ExpansionTile(
+          title: SizedBox(
+            child: Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: Text(
+                    fieldData.id.toString(),
+                    style: const TextStyle(
+                      fontFamily: 'SofiaSans',
+                      fontSize: 25,
+                      color: Colors.black,
+                      fontWeight: FontWeight.normal,
+                    ),
                   ),
-                  [
-                    buildInfoContainer(
-                      "Type",
-                      fieldData.type.name.toLowerCase(),
-                      MediaQuery.of(context).size.width,
+                ),
+                Flexible(
+                  child: Container(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: Text(
+                      fieldData.name,
+                      overflow: TextOverflow.fade,
+                      softWrap: false,
+                      style: const TextStyle(
+                        fontFamily: 'SofiaSans',
+                        fontSize: 25,
+                        color: Colors.black,
+                        fontWeight: FontWeight.normal,
+                      ),
                     ),
-                    fieldData.unit.id != -1
-                        ? buildInfoContainer(
-                            "Unit",
-                            fieldData.unit.name,
-                            MediaQuery.of(context).size.width,
-                          )
-                        : Container(),
-                    buildDeleteEditButtonRow(
-                      () => _onDeletePressed(fieldData, provider),
-                      () => _editUser(fieldData, provider),
-                    ),
-                  ]),
+                  ),
+                )
+              ],
+            ),
+          ),
+          collapsedBackgroundColor: Colors.white,
+          tilePadding: const EdgeInsets.all(20),
+          childrenPadding: const EdgeInsets.all(0),
+          children: [
+            buildInfoContainer(
+              "Type",
+              fieldData.type.name.toLowerCase(),
+              MediaQuery.of(context).size.width,
+            ),
+            fieldData.unit.id != -1
+                ? buildInfoContainer(
+              "Unit",
+              fieldData.unit.name,
+              MediaQuery.of(context).size.width,
             )
-            .toList(),
+                : Container(),
+            buildDeleteEditButtonRow(
+                  () => _onDeletePressed(fieldData),
+                  () => _onEditPressed(fieldData),
+            ),
+          ],
+        ),
       );
 
-  void _onDeletePressed(FieldData fieldData, FieldsListProvider provider) {
+  FloatingActionButton _buildAddButton() => FloatingActionButton(
+    onPressed: () => showAddFieldModal(
+      context,
+      widget.repository,
+      addField,
+      false,
+      null,
+    ),
+    backgroundColor: adminGreenColor,
+    child: const Icon(Icons.add),
+  );
+
+  void _onDeletePressed(FieldData fieldData) {
     showAlertDialog(
       context,
       'Delete ' + fieldData.name,
       "You are about to delete group with all of its' saved permissions.",
-      () => deleteField(fieldData.id, provider),
+          () => deleteField(fieldData.id),
     );
   }
 
-  void _editUser(FieldData fieldData, FieldsListProvider provider) {
-    //todo: next task
+  void _onEditPressed(FieldData fieldData) {
+    showAddFieldModal(
+      context,
+      widget.repository,
+      editField,
+      true,
+      fieldData,
+    );
   }
 
-  void deleteField(int id, FieldsListProvider provider) async {
+  void editField(
+      int id,
+      String fieldName,
+      String unitName,
+      FieldType fieldType,
+      ) async {
+    await widget.repository
+        .editField(id, fieldName, unitName, fieldType)
+        .then(
+          (newField) => {
+        provider.editField(newField),
+        buildSnackbar(
+          "Field edited",
+          context,
+          duration: 3,
+          color: adminGreenColor,
+          height: 65,
+        )
+      },
+    )
+        .catchError((error) {
+      buildSnackbar(
+        'Cannot add field',
+        context,
+        duration: 10,
+        height: 65,
+      );
+    });
+  }
+
+  void deleteField(int id) async {
     await widget.repository
         .deleteField(id)
         .then(
           (value) => {
-            if (value)
-              {
-                provider.delete(id),
-              }
-          },
-        )
+        if (value)
+          {
+            provider.delete(id),
+            buildSnackbar(
+              "Field deleted",
+              context,
+              duration: 3,
+              color: adminGreenColor,
+              height: 65,
+            )
+          }
+      },
+    )
         .catchError((error) {
-      buildSnackbar('Cannot delete field', context);
+      buildSnackbar(
+        'Cannot delete field',
+        context,
+        duration: 10,
+        height: 65,
+      );
+    });
+  }
+
+  void addField(
+      int? id,
+      String fieldName,
+      String unitName,
+      FieldType fieldType,
+      ) async {
+    await widget.repository
+        .addField(fieldName, unitName, fieldType)
+        .then(
+          (newField) => {
+        provider.addNewField(newField),
+        buildSnackbar(
+          "Field added",
+          context,
+          duration: 3,
+          color: adminGreenColor,
+          height: 65,
+        )
+      },
+    )
+        .catchError((error) {
+      buildSnackbar(
+        'Cannot add field',
+        context,
+        duration: 10,
+        height: 65,
+      );
     });
   }
 
--- frontend/lib/Views/AdminPage/groups/show_add_user_modal.dart
+++ frontend/lib/Views/AdminPage/groups/show_add_user_modal.dart
@@ -32,7 +32,7 @@ void showAddUserModal(
             } else {
               return SizedBox(
                 width: MediaQuery.of(context).size.width * 0.7,
-                height: MediaQuery.of(context).size.width * 0.2,
+                height: MediaQuery.of(context).size.height * 0.3,
                 child: Scaffold(
                   appBar: AppBar(
                     shadowColor: Colors.transparent,
